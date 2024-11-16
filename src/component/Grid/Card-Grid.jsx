3910339639885cfb39b970faac41caaee33c11ce
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-11-16 11:28:21 +0700

    change color to main theme and add more machanic to date and "Load More" button
    - app\crud\event.py we change event sort
    - src\component\Buttons\Fillter-Btn.jsx change color to blue and add hover
    - src\component\Buttons\View-All-Btn.jsx change color to blue and add hover
    - src\component\Cards\Event-Card.jsx add machanic to deal with time
    - src\component\Grid\Card-Grid.jsx change how it flex, color and machanic for "Load More" button
    - src\component\Topic-Header.jsx change color

--- src/component/Grid/Card-Grid.jsx
+++ src/component/Grid/Card-Grid.jsx
@@ -8,87 +8,94 @@ import { useInfiniteFetch } from "api/custom-hooks.js";
 import EventCard from "component/Cards/Event-Card.jsx";
 
 function GridCards({ toFetchedData = {}, url = "" }) {
-  const recentNewsGridQuery = useInfiniteFetch({
-    id: toFetchedData.id,
-    url: url,
-    pageSize: toFetchedData.pageSize,
-  });
-  const { data, isLoading, isError } = recentNewsGridQuery;
-  const topic = data ? Object.keys(data.pages[0][0])[0] : undefined;
+    const recentNewsGridQuery = useInfiniteFetch({
+        id: toFetchedData.id,
+        url: url,
+        pageSize: toFetchedData.pageSize,
+    });
+    const { data, isLoading, isError } = recentNewsGridQuery;
+    const topic = data ? Object.keys(data.pages[0][0])[0] : undefined;
 
-  // Helper function to chunk array into groups of 5
-  const chunkArray = (arr, size) => {
-    const result = [];
-    for (let i = 0; i < arr.length; i += size) {
-      result.push(arr.slice(i, i + size));
-    }
-    return result;
-  };
+    // Helper function to chunk array into groups of 5
+    const chunkArray = (arr, size) => {
+        const result = [];
+        for (let i = 0; i < arr.length; i += size) {
+            result.push(arr.slice(i, i + size));
+        }
+        return result;
+    };
 
-  return (
-    <section className="flex overflow-hidden flex-col px-16 py-28 w-full bg-sky-100 max-md:px-5 max-md:py-24 max-md:max-w-full">
-      <div className="flex flex-wrap gap-10 justify-between items-end w-full max-md:max-w-full">
-        <div className="flex flex-col text-black min-w-[240px] w-[768px] max-md:max-w-full"></div>
-        <div className="flex items-center gap-4">
-          <FilterButton />
-          <ViewAllButton />
-        </div>
-      </div>
-      <div className="flex flex-col mt-6 w-full max-md:mt-5 max-md:max-w-full">
-        {/* Reduced margin-top */}
-        <div className="box-border flex relative flex-col shrink-0">
-          <div className="w-full max-md:max-w-full mt-4">
-            {!isLoading ? (
-              data.pages.map((itemArr, pageIndex) =>
-                chunkArray(itemArr, 4).map((row, rowIndex) => (
-                  <div
-                    key={`${pageIndex}-${rowIndex}`}
-                    className="flex gap-4 mb-6 flex-wrap"
-                  >
-                    {row.map((item, itemIndex) => {
-                      const topicData = item[topic];
-                      const resolvedID =
-                        topic === "News"
-                          ? topicData.NID
-                          : topic === "Laboratory"
-                          ? topicData.LID
-                          : topic === "Research"
-                          ? topicData.RID
-                          : topic === "Publication"
-                          ? topicData.PID
-                          : null;
-                      return topic !== "Event" ? (
-                        <NewsCard
-                          key={`${pageIndex}-${rowIndex}-${itemIndex}`}
-                          {...item[topic]}
-                          type={topic}
-                          ID={resolvedID}
-                        />
-                      ) : topic === "Event" ? (
-                        <EventCard
-                          key={`${pageIndex}-${rowIndex}-${itemIndex}`}
-                          {...item[topic]}
-                          ID={resolvedID}
-                        />
-                      ) : null;
-                    })}
-                  </div>
-                ))
-              )
-            ) : (
-              <div>Loading...</div>
+    // Calculate total number of items
+    const totalItems = data
+        ? data.pages.reduce((acc, page) => acc + page.length, 0)
+        : 0;
+
+    return (
+        <section className="flex overflow-hidden flex-col px-16 w-full bg-gray-100 max-md:px-5 max-md:py-24 max-md:max-w-full">
+            <div className="flex flex-wrap gap-10 justify-between items-end w-full max-md:max-w-full">
+                <div className="flex flex-col text-black min-w-[240px] w-[768px] max-md:max-w-full"></div>
+                <div className="flex items-center gap-4">
+                    <FilterButton className="hover:bg-gray-200 " />
+                    <ViewAllButton className="hover:bg-gray-200" />
+                </div>
+            </div>
+            <div className="flex flex-col mt-6 w-full max-md:mt-5 max-md:max-w-full items-center">
+                {/* Reduced margin-top */}
+                <div className="box-border flex relative flex-col shrink-0">
+                    <div className="w-full max-md:max-w-full mt-4">
+                        {!isLoading ? (
+                            data.pages.map((itemArr, pageIndex) =>
+                                chunkArray(itemArr, 4).map((row, rowIndex) => (
+                                    <div
+                                        key={`${pageIndex}-${rowIndex}`}
+                                        className="flex gap-6 mb-6 flex-wrap "
+                                    >
+                                        {row.map((item, itemIndex) => {
+                                            const topicData = item[topic];
+                                            const resolvedID =
+                                                topic === "News"
+                                                    ? topicData.NID
+                                                    : topic === "Laboratory"
+                                                    ? topicData.LID
+                                                    : topic === "Research"
+                                                    ? topicData.RID
+                                                    : topic === "Publication"
+                                                    ? topicData.PID
+                                                    : null;
+                                            return topic !== "Event" ? (
+                                                <NewsCard
+                                                    key={`${pageIndex}-${rowIndex}-${itemIndex}`}
+                                                    {...item[topic]}
+                                                    type={topic}
+                                                    ID={resolvedID}
+                                                    className="shadow-lg rounded-lg p-4"
+                                                />
+                                            ) : topic === "Event" ? (
+                                                <EventCard
+                                                    key={`${pageIndex}-${rowIndex}-${itemIndex}`}
+                                                    {...item[topic]}
+                                                    ID={resolvedID}
+                                                    className="shadow-lg rounded-lg p-4"
+                                                />
+                                            ) : null;
+                                        })}
+                                    </div>
+                                ))
+                            )
+                        ) : (
+                            <div>Loading...</div>
+                        )}
+                    </div>
+                </div>
+            </div>
+            {!isError && totalItems >= 8 && (
+                <button className="px-8 py-5 mt-8 w-full text-lg bg-blue-500 text-white border rounded-2xl  border-solid max-md:px-5 max-md:max-w-full hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105">
+                    Load More
+                </button>
             )}
-          </div>
-        </div>
-      </div>
-      {!isError && (
-        <button className="px-8 py-5 mt-8 w-full text-lg text-black bg-white rounded-2xl border border-black border-solid max-md:px-5 max-md:max-w-full">
-          Load More
-        </button>
-      )}
-      {/* Pagination and navigation controls */}
-    </section>
-  );
+            {/* Pagination and navigation controls */}
+        </section>
+    );
 }
 
 export default GridCards;
--- src/routes/user/Landing-Page.jsx
+++ src/routes/user/Landing-Page.jsx
@@ -19,156 +19,156 @@ import MyFormComponent from "../../component/etc/exampleForm";
 import LoginComp from "component/etc/example-login";
 import CSSTailwind from "./CSS-Tailwind-Page";
 function MainPages() {
-  // axios
-  //   .get(
-  //     "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980"
-  //   )
-  //   .then((response) => {
-  //     console.log(response.data);
-  //   });
+    // axios
+    //   .get(
+    //     "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980"
+    //   )
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
 
-  // console.log(
-  //   getData(
-  //     "http://localhost:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980"
-  //   )
-  // );
-  // const a = [
-  //   [
-  //     {
-  //       id: "abc",
-  //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
-  //       type: "n",
-  //     },
-  //     {
-  //       id: "jsf",
-  //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
-  //       type: "n",
-  //     },
-  //     {
-  //       id: "jpj",
-  //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
-  //       type: "n",
-  //     },
-  //   ],
-  //   [
-  //     {
-  //       id: "oijf",
-  //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
-  //       type: "n",
-  //     },
-  //     {
-  //       id: "asd",
-  //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
-  //       type: "n",
-  //     },
-  //   ],
-  // ];
-  // console.log(DataFetcherQueue(a));
-  const location = useLocation();
-  switch (location.pathname) {
-    // case "/":
-    //   return (
-    //     <>
-    //       <Modals />
-    //       <LoginComp />
-    //       <MyFormComponent />
-    //       <TableComponent />
-    //       <HeroBox />
-    //       <DividingRows />
-    //       <RecentNews
-    //         toFetchedData={exampleToFetchData.recentNews}
-    //         topic="news"
-    //       />
-    //       <RecentEvents
-    //         toFetchedData={exampleToFetchData.recentEvents}
-    //         topic="events"
-    //       />
-    //     </>
-    //   );
-    case "/":
-      return (
-        <>
-          <CSSTailwind />
-        </>
-      );
+    // console.log(
+    //   getData(
+    //     "http://localhost:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980"
+    //   )
+    // );
+    // const a = [
+    //   [
+    //     {
+    //       id: "abc",
+    //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
+    //       type: "n",
+    //     },
+    //     {
+    //       id: "jsf",
+    //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
+    //       type: "n",
+    //     },
+    //     {
+    //       id: "jpj",
+    //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
+    //       type: "n",
+    //     },
+    //   ],
+    //   [
+    //     {
+    //       id: "oijf",
+    //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
+    //       type: "n",
+    //     },
+    //     {
+    //       id: "asd",
+    //       url: "http://127.0.0.1:8000/user/event/thumbnail?laboratory_id=ad7edead-e775-48df-bde7-7f334c8c0980",
+    //       type: "n",
+    //     },
+    //   ],
+    // ];
+    // console.log(DataFetcherQueue(a));
+    const location = useLocation();
+    switch (location.pathname) {
+        // case "/":
+        //   return (
+        //     <>
+        //       <Modals />
+        //       <LoginComp />
+        //       <MyFormComponent />
+        //       <TableComponent />
+        //       <HeroBox />
+        //       <DividingRows />
+        //       <RecentNews
+        //         toFetchedData={exampleToFetchData.recentNews}
+        //         topic="news"
+        //       />
+        //       <RecentEvents
+        //         toFetchedData={exampleToFetchData.recentEvents}
+        //         topic="events"
+        //       />
+        //     </>
+        //   );
+        case "/":
+            return (
+                <>
+                    <CSSTailwind />
+                </>
+            );
 
-    case "/about":
-      return (
-        <>
-          <TopicAndImage />
-          <AboutDescription />
-          <RecentNews toFetchedData={exampleToFetchData.recentNews} />
-          <RecentEvents
-            toFetchedData={exampleToFetchData.recentEvents}
-            listData={eventItems}
-            topic="events"
-          />
-        </>
-      );
-    case "/events":
-      return (
-        <>
-          <TopicHeaderText topic="Events" />
-          <GridCards
-            toFetchedData={exampleToFetchData.recentGridEvents}
-            topic="events"
-            url="http://127.0.0.1:8000/user/event/thumbnail?"
-          />
-        </>
-      );
-    case "/news":
-      return (
-        <>
-          <TopicHeaderText topic="News" />
-          <GridCards
-            toFetchedData={exampleToFetchData.recentGridNews}
-            topic="news"
-            url="http://127.0.0.1:8000/user/news/thumbnail?"
-          />
-        </>
-      );
-    case "/publications":
-      return (
-        <>
-          <TopicHeaderText topic="Publications" />
-          <GridCards
-            toFetchedData={exampleToFetchData.recentGridNews}
-            url="http://127.0.0.1:8000/user/publication/thumbnail?"
-          />
-        </>
-      );
-    case "/research":
-      return (
-        <>
-          <TopicHeaderText topic="Research" />
-          <GridCards
-            toFetchedData={exampleToFetchData.recentGridResearch}
-            url="http://127.0.0.1:8000/user/research/thumbnail?"
-          />
-        </>
-      );
-    case "/laboratory":
-      return (
-        <>
-          <TopicHeaderText topic="Laboratory" />
-          <GridCards
-            toFetchedData={exampleToFetchData.recentGridLaboratory}
-            url="http://127.0.0.1:8000/user/laboratory/thumbnail?"
-          />
-        </>
-      );
-    default:
-      <></>;
-  }
-  // { index: true, element: <MainPages /> },
-  // { path: "about", element: <MainPages /> },
-  // { path: "events", element: <MainPages /> },
-  // { path: "labs", element: <MainPages /> },
-  // { path: "news", element: <MainPages /> },
-  // { path: "publications", element: <MainPages /> },
-  // { path: "research", element: <MainPages /> },
-  // { path: "events", element: <MainPages /> },
-  // { path: "people", element: <MainPages /> },
-  // { path: "labs/:labID", element: <DynamicLabPage /> },
+        case "/about":
+            return (
+                <>
+                    <TopicAndImage />
+                    <AboutDescription />
+                    <RecentNews toFetchedData={exampleToFetchData.recentNews} />
+                    <RecentEvents
+                        toFetchedData={exampleToFetchData.recentEvents}
+                        listData={eventItems}
+                        topic="events"
+                    />
+                </>
+            );
+        case "/events":
+            return (
+                <>
+                    <TopicHeaderText topic="Events" />
+                    <GridCards
+                        toFetchedData={exampleToFetchData.recentGridEvents}
+                        topic="events"
+                        url="http://127.0.0.1:8000/user/event/thumbnail?"
+                    />
+                </>
+            );
+        case "/news":
+            return (
+                <>
+                    <TopicHeaderText topic="News" />
+                    <GridCards
+                        toFetchedData={exampleToFetchData.recentGridNews}
+                        topic="news"
+                        url="http://127.0.0.1:8000/user/news/thumbnail?"
+                    />
+                </>
+            );
+        case "/publications":
+            return (
+                <>
+                    <TopicHeaderText topic="Publications" />
+                    <GridCards
+                        toFetchedData={exampleToFetchData.recentGridNews}
+                        url="http://127.0.0.1:8000/user/publication/thumbnail?"
+                    />
+                </>
+            );
+        case "/research":
+            return (
+                <>
+                    <TopicHeaderText topic="Research" />
+                    <GridCards
+                        toFetchedData={exampleToFetchData.recentGridResearch}
+                        url="http://127.0.0.1:8000/user/research/thumbnail?"
+                    />
+                </>
+            );
+        case "/laboratory":
+            return (
+                <>
+                    <TopicHeaderText topic="Laboratory" />
+                    <GridCards
+                        toFetchedData={exampleToFetchData.recentGridLaboratory}
+                        url="http://127.0.0.1:8000/user/laboratory/thumbnail?"
+                    />
+                </>
+            );
+        default:
+            <></>;
+    }
+    // { index: true, element: <MainPages /> },
+    // { path: "about", element: <MainPages /> },
+    // { path: "events", element: <MainPages /> },
+    // { path: "labs", element: <MainPages /> },
+    // { path: "news", element: <MainPages /> },
+    // { path: "publications", element: <MainPages /> },
+    // { path: "research", element: <MainPages /> },
+    // { path: "events", element: <MainPages /> },
+    // { path: "people", element: <MainPages /> },
+    // { path: "labs/:labID", element: <DynamicLabPage /> },
 }
 export default MainPages;
